b9e0a1363796700c29c8de27d7260062c4c4f1e9
Eugene <klev.evzen@gmail.com>
2023-01-03 16:47:58 +0100

    add randomAnimals and render them

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import AnimalShow from "./AnimalShow";
 import { useState } from "react";
+import AnimalShow from "./AnimalShow";
+
+function getRandomAnimal() {
+  const animals = ["bird", "cat", "cow", "dog", "gator", "horse"];
+
+  return animals[Math.floor(Math.random() * animals.length)];
+}
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [animals, setAnimals] = useState([]);
 
-  const handleClick = () => {
-    setCount(prev => prev + 1);
-  };
+  const handleClick = () => setAnimals([...animals, getRandomAnimal()]);
+
+  const renderedAnimals = animals.map((animal, index) => {
+    return <AnimalShow key={index} type={animal} />;
+  });
 
   return (
     <div>
       <button onClick={handleClick}>Add Animal</button>
-      <div>Number of animals: {count}</div>
+      <div>{renderedAnimals}</div>
     </div>
   );
 }
